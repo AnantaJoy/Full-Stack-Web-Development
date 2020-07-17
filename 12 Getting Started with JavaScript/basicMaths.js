c4aa40ca197af3971a56ae91b5f87b5cf950cc45
Ananta Asim Joy <anantajoy007@gmail.com>
2020-07-17 20:12:47 +0600

    js conditional statement added

--- 12 Getting Started with JavaScript/basicMaths.js
+++ 12 Getting Started with JavaScript/basicMaths.js
@@ -18,3 +18,25 @@ console.log(totalMulti);
 // division of two numbers
 var totalDivision = num1 / num2;
 console.log(totalDivision.toFixed(3));
+
+// absolute value
+var value1 = -16.4;
+var  absoluteValue = Math.abs(value1);
+console.log(absoluteValue);
+
+// around  a value
+var value2 = 18.5
+var roundValue = Math.round(value2);
+console.log(roundValue);
+
+// floor a value
+var floorValue = Math.floor(value2);
+console.log(floorValue);
+
+// ceil a value
+console.log(Math.ceil(3.1));
+
+// random vale
+var randomNumber = Math.random()*100;
+var randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
---
+++ 12 Getting Started with JavaScript/conditionalStatement.js
@@ -0,0 +1,22 @@
+// odd or even
+var num1 = 2
+if (num1%2==0){
+    console.log('Even')
+}
+else {
+    console.log('Odd')
+}
+
+
+// lunch menu decision
+var pocketMoney = 100;
+
+if (pocketMoney>200){
+    console.log('Biriyani and Drinks');
+}
+else if(100<pocketMoney<200) {
+    console.log('Plain Rice and chicken');   
+}
+else {
+    console.log('Rice and Vegetable');
+}
